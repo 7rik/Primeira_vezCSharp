a8f025a5ef2fea377c6a5ae21613f3e24911ee9f
bezer <bezer@ERIK>
2022-08-30 18:12:12 -0300

    Adicionar arquivos de projeto.

---
+++ Primerio/Program.cs
@@ -0,0 +1,11 @@
+﻿const string textoolamundo = "Ola mundo, meu primeiro programa em c#, feito por ";
+string nomecompleto;
+int idade;
+
+Console.WriteLine("Qual o seu nome completo?");
+nomecompleto = Console.ReadLine().ToString();
+
+Console.WriteLine("Qual sua idade?");
+idade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(textoolamundo + nomecompleto + " que tem " + idade +" anos");
